ceb34901bd6e40b43c321922677a8a6bd211e6a2
Emma Stotz <stotz.emma@gmail.com>
2019-12-04 00:02:53 -0500

    Back end again.

--- meread/client/src/utils/API.js
+++ meread/client/src/utils/API.js
@@ -2,15 +2,15 @@ import axios from "axios";
 
 export default {
   getTemplates: function() {
-    return axios.get("/api/templates");
+    return axios.get("/Generator/api/templates");
   },
   getTemplate: function(id) {
-    return axios.get("/api/templates/" + id);
+    return axios.get("/Generator/api/templates/" + id);
   },
   deleteTemplate: function(id) {
-    return axios.delete("/api/templates/" + id);
+    return axios.delete("/Generator/api/templates/" + id);
   },
   saveTemplate: function(templateData) {
-    return axios.post("/api/templates", templateData);
+    return axios.post("/Generator/api/templates", templateData);
   }
 };
